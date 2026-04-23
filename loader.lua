fe1e29d9b8e00e15320c4a4508e113b13640ec05
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2026-04-22 20:18:53 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,8 @@
+local SCRIPTS = {
+    [2682938349] = "https://api.luarmor.net/files/v4/loaders/457b74fb02beafe37cb808f6fa2be7f8.lua" -- Wild Horse Islands
+}
 
+local GAME_ID = game.GameId
+local SCRIPT_URL = SCRIPTS[GAME_ID]
+
+loadstring(game:HttpGet(SCRIPT_URL))()
